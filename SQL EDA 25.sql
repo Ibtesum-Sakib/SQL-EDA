1588608342dfba3f94c4d23dea8a734e0b226598
Ibtesum <ibtesum38@gmail.com>
2025-08-27 00:55:09 +0200

    adding recent layoff till April 2025

--- SQL EDA 25.sql
+++ SQL EDA 25.sql
@@ -31,3 +31,52 @@ SELECT country, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY country
 ORDER BY 2 DESC;
+
+SELECT YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC;
+
+
+SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `Month`
+ORDER BY 1 ASC;
+
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `Month`,SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `Month`
+ORDER BY 1 ASC
+)
+SELECT `Month`, total_off, SUM(total_off) OVER(ORDER BY `Month`) AS Rolling_Total
+FROM Rolling_Total;
+
+SELECT company, SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
+
+WITH Company_year (company, years, total_laid_off) AS
+(
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+),Company_year_rank AS
+(SELECT *, 
+DENSE_RANK () OVER(PARTITION BY years ORDER BY total_laid_off DESC) AS Ranking
+FROM Company_year
+WHERE years IS NOT NULL
+)
+SELECT *
+FROM Company_year_rank
+WHERE Ranking <= 5 ;
